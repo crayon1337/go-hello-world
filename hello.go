6650f1867f78d5349a45693f7147965c1baecd65
crayon1337 <darkspear123@outlook.com>
2023-01-27 20:44:34 +0200

    Adding error handling

--- hello.go
+++ hello.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"fmt"
 
 	"crayon/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Mohammed")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(message)
 }
